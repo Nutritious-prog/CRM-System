822f57592032f3f76211893044c00ee54c51cfc8
Artur Wlodarczyk <artur.wlodarczyk12@gmail.com>
2022-01-26 12:06:35 +0100

    Connection testing DB Servlet added

--- src/nutritious/prog/testdb/TestDbServlet.java
+++ src/nutritious/prog/testdb/TestDbServlet.java
@@ -1,11 +1,15 @@
 package nutritious.prog.testdb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
 
 /**
  * Servlet implementation class TestDbServlet
@@ -18,8 +22,35 @@ public class TestDbServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// connection vars
+		
+		String user = "springstudent";
+		String pass = "springstudent";
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&characterEncoding=latin1&useConfigs=maxPerformance";
+		String driver = "com.mysql.jdbc.Driver";
+		
+		// db connection
+		
+		try {
+			
+			PrintWriter out = response.getWriter();
+			
+			out.println("Connecting to database: " + jdbcUrl);
+			
+			Class.forName(driver);
+			
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			out.println("SUCCESS!!!");
+			
+			myConn.close();
+			
+		} catch (Exception e) {
+			System.out.println("Something went wrong");
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
 	}
 
 }
